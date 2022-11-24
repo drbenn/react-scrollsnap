452b668176d055e5b40022e1ada6b9c4cc5a04d3
ratlord <bennett.daniel@gmail.com>
2022-11-24 16:25:28 -0500

    bg images passed as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import styles from "./styles/App.module.scss"
 
 import Basic from './components/basic.component';
 
 function App() {
+  const bg1 = 'bg-1'
 
   return (
     <div className={`container ${styles.container}`}>
-      <Basic />
-      <Basic />
-      <Basic />
+      {/* passing props https://beta.reactjs.org/learn/passing-props-to-a-component */}
+      <Basic 
+        image={`url(/src/assets/bg-1.jpg)`}
+        headline={`Basic Scrollsnap 1!`}
+        />
+      <Basic 
+        image={`url(/src/assets/bg-2.jpg)`}
+        headline={`Basic Scrollsnap 2!`}
+      />
+      <Basic 
+        image={`url(/src/assets/bg-3.jpg)`}
+        headline={`Basic Scrollsnap 3!`}
+      />
 
     </div>
   )
--- src/components/basic.component.jsx
+++ src/components/basic.component.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef } from "react";
 import styles from './basic.module.scss'
+import BackgroundImage from '../assets/bg-1.jpg';
 
 
-export default function Basic() {
+export default function Basic({ image, headline }) {
 
   return(
-    <div className={styles.section}>
+    //figure out passing props later
+    <div className={styles.section} 
+    // style={{backgroundImage: `url('/src/assets/bg-1.jpg`}}>
+      style={{backgroundImage: `${image}`}}>
+    {/* <div className={styles.section}> */}
       <div className={styles.copy}>
-        <h2>Basic Scrollsnap</h2>
+        <h2>{headline}</h2>
       </div>
       {/* Image tag not available and related to Next.js & React Native?? */}
       {/* <Image src={`/src/assets/bg.jpg`} layout={ `fill`} /> */}
